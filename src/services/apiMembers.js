cba008537a66b925215d0ca7e2463331fcd5c9fe
puffLive <44915404+puffLive@users.noreply.github.com>
2025-06-02 13:33:09 -0400

    updated api call to call the public url to get member data.

--- src/services/apiMembers.js
+++ src/services/apiMembers.js
@@ -4,9 +4,11 @@ import { PE_API_BASE_URL } from "../config/config";
 
 export async function getMembers() {
   try {
-    return await axios.get(`${PE_API_BASE_URL}api/v1/members`).then((res) => {
-      return res.data.data.data;
-    });
+    return await axios
+      .get(`${PE_API_BASE_URL}api/v1/members/public`)
+      .then((res) => {
+        return res.data.data.data;
+      });
   } catch (error) {
     console.log("getMembers error: ", error);
     throw new Error("Members could not be loaded");
